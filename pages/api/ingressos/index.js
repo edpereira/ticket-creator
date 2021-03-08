06014fcbc8e77e90e06a12d49f56507c167a9702
Eduardo <eduardo.pereira.ext@panonline.com.br>
2021-03-08 01:24:53 -0300

    Teste env

--- pages/api/ingressos/index.js
+++ pages/api/ingressos/index.js
@@ -7,6 +7,9 @@ import QRCode from "qrcode";
 
 async function email(ticket) {
     try {
+        console.log("EMAIL")
+        console.log(process.env.EMAIL);
+        console.log(process.env.SENHA);
         var transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
